534f1c009201f9e1c708455ebbde11028b7c5174
Jerry Tang <jerrytang16@gmail.com>
2023-09-29 14:43:29 -0400

    edited init.py for better logging

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -10,10 +10,11 @@ from flask_session import Session
 
 app = Flask(__name__)
 app.config.from_object(Config)
-app.logger.setLevel(logging.WARNING)
+wsgi_app = app.wsgi_app
+app.logger.setLevel(logging.INFO)
 streamHandler = logging.StreamHandler()
-streamHandler.setLevel(logging.WARNING)
-app.logger.addHandler(streamHandler) 
+streamHandler.setLevel(logging.INFO)
+app.logger.addHandler(streamHandler)
 
 Session(app)
 db = SQLAlchemy(app)
